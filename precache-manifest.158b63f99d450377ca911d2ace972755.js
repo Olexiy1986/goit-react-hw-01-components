9616d6126c4bad5f28b053d6f6a9e09847132a5a
Oleksiy Shovkun <ssapfir1986@ukr.net>
2020-06-06 11:17:19 +0300

    Updates

---
+++ precache-manifest.158b63f99d450377ca911d2ace972755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ccc96e55fddea8f62d45fbb9c122f0",
+    "url": "/ghpages-deployment/index.html"
+  },
+  {
+    "revision": "8017afacfb655058b5ee",
+    "url": "/ghpages-deployment/static/css/main.9a69f615.chunk.css"
+  },
+  {
+    "revision": "99919af9b0d94aaa0bb8",
+    "url": "/ghpages-deployment/static/js/2.befd264b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages-deployment/static/js/2.befd264b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8017afacfb655058b5ee",
+    "url": "/ghpages-deployment/static/js/main.a8213b55.chunk.js"
+  },
+  {
+    "revision": "41bba7ef36cc5cc32a05",
+    "url": "/ghpages-deployment/static/js/runtime-main.6d1c2870.js"
+  }
+]);
